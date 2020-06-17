2cf26225278cc2c51284e3206101c7807be97e3d
Fahad Hossain <set.name.fahad@gmail.com>
2020-06-17 09:40:22 +0600

    add modali

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,37 @@
-import React, { Component } from 'react';
-import logo from './react.svg';
-import './Home.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import logo from "./react.svg";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import Modali, { useModali } from "modali";
 
 function Home() {
+  const [modal, toggleModal] = useModali({
+    buttons: [
+      <Modali.Button
+        label="Cancel"
+        isStyleCancel
+        onClick={() => toggleCompleteModal()}
+      />,
+    ],
+  });
   return (
-    <div className="Home">
-      <div className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <h2>Welcome to After.js</h2>
+    <>
+      <Modali.Modal {...modal}></Modali.Modal>
+      <div className="Home">
+        <div className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h2>Welcome to After.js</h2>
+        </div>
+        <p className="Home-intro">
+          To get started, edit <code>src/Home.js</code> or{" "}
+          <code>src/About.js</code>and save to reload.
+        </p>
+        <Link to="/about">About -&gt;</Link>
+        <div>
+          <button onClick={toggleModal}>Toggle Modal</button>
+        </div>
       </div>
-      <p className="Home-intro">
-        To get started, edit <code>src/Home.js</code> or{' '}
-        <code>src/About.js</code>and save to reload.
-      </p>
-      <Link to="/about">About -></Link>
-    </div>
+    </>
   );
 }
 
@@ -27,7 +43,7 @@ Home.getInitialProps = async ({
   location,
   ...ctx
 }) => {
-  return { whatever: 'stuff' };
+  return { whatever: "stuff" };
 };
 
 export default Home;
